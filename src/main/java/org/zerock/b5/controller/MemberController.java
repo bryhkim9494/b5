29de098ce269100d8fb0f30b7d11115ba6bac85a
brbr <bryhkim9494@gmail.com>
2023-06-24 23:18:22 +0900

    Initial commit

---
+++ src/main/java/org/zerock/b5/controller/MemberController.java
@@ -0,0 +1,33 @@
+package org.zerock.b5.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/member")
+public class MemberController {
+
+    @PreAuthorize("permitAll")
+    @GetMapping("/signup")
+    public void signup(){
+        log.info("signup-------------------");
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/mypage")
+    public void mypage(){
+        log.info("mypage-------------------");
+    }
+
+    @PreAuthorize("permitAll")
+    @GetMapping("/signin")
+    public void signin(){
+        log.info("signin-------------------");
+    }
+}
